94c4c38f15fbb47ca5ee46dfa305f71ef0fec354
mu_tomoya <muton.tm.714@gmail.com>
2023-04-06 17:59:09 +0900

    Stripe最新版対応

--- app/Http/Controllers/User/CartController.php
+++ app/Http/Controllers/User/CartController.php
@@ -20,25 +20,25 @@ class CartController extends Controller
         $products = $user->products;
         $totalPrice = 0;
 
-        foreach($products as $product){
+        foreach ($products as $product) {
             $totalPrice += $product->price * $product->pivot->quantity;
         }
 
         //dd($products, $totalPrice);
 
-        return view('user.cart', 
-            compact('products', 'totalPrice'));
+        return view(
+            'user.cart',
+            compact('products', 'totalPrice')
+        );
     }
 
     public function add(Request $request)
     {
         $itemInCart = Cart::where('product_id', $request->product_id)
-        ->where('user_id', Auth::id())->first();
-
-        if($itemInCart){
+            ->where('user_id', Auth::id())->first();
+        if ($itemInCart) {
             $itemInCart->quantity += $request->quantity;
             $itemInCart->save();
-
         } else {
             Cart::create([
                 'user_id' => Auth::id(),
@@ -46,46 +46,44 @@ class CartController extends Controller
                 'quantity' => $request->quantity
             ]);
         }
-        
         return redirect()->route('user.cart.index');
     }
 
     public function delete($id)
     {
         Cart::where('product_id', $id)
-        ->where('user_id', Auth::id())
-        ->delete();
+            ->where('user_id', Auth::id())
+            ->delete();
 
         return redirect()->route('user.cart.index');
     }
 
     public function checkout()
     {
-        
-
         $user = User::findOrFail(Auth::id());
         $products = $user->products;
-        
         $lineItems = [];
-        foreach($products as $product){
+        foreach ($products as $product) {
             $quantity = '';
             $quantity = Stock::where('product_id', $product->id)->sum('quantity');
-
-            if($product->pivot->quantity > $quantity){
+            if ($product->pivot->quantity > $quantity) {
                 return redirect()->route('user.cart.index');
             } else {
                 $lineItem = [
-                    'name' => $product->name,
-                    'description' => $product->information,
-                    'amount' => $product->price,
-                    'currency' => 'jpy',
+                    "price_data" => [
+                        'unit_amount' => $product->price,
+                        'currency' => 'jpy',
+                        'product_data' => [
+                            'name' => $product->name,
+                            'description' => $product->information
+                        ],
+                    ],
                     'quantity' => $product->pivot->quantity,
                 ];
-                array_push($lineItems, $lineItem);    
+                array_push($lineItems, $lineItem);
             }
         }
-        // dd($lineItems);
-        foreach($products as $product){
+        foreach ($products as $product) {
             Stock::create([
                 'product_id' => $product->id,
                 'type' => \Constant::PRODUCT_LIST['reduce'],
@@ -105,8 +103,10 @@ class CartController extends Controller
 
         $publicKey = env('STRIPE_PUBLIC_KEY');
 
-        return view('user.checkout', 
-            compact('session', 'publicKey'));
+        return view(
+            'user.checkout',
+            compact('session', 'publicKey')
+        );
     }
 
     public function success()
@@ -117,8 +117,7 @@ class CartController extends Controller
         $user = User::findOrFail(Auth::id());
 
         SendThanksMail::dispatch($products, $user);
-        foreach($products as $product)
-        {
+        foreach ($products as $product) {
             SendOrderedMail::dispatch($product, $user);
         }
         // dd('ユーザーメール送信テスト');
@@ -132,7 +131,7 @@ class CartController extends Controller
     {
         $user = User::findOrFail(Auth::id());
 
-        foreach($user->products as $product){
+        foreach ($user->products as $product) {
             Stock::create([
                 'product_id' => $product->id,
                 'type' => \Constant::PRODUCT_LIST['add'],
